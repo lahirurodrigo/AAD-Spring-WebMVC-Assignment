c51e0170ff0b5598e3a6b9ae3ce526023335773a
lahirurodrigo <kmlahirurodrigo@gmail.com>
2024-10-16 12:55:54 +0530

    Implement SLF4J logging and configure Logback for LoginController

--- src/main/java/lk/ijse/aadspringwebmvcassignment/config/WebAppConfig.java
+++ src/main/java/lk/ijse/aadspringwebmvcassignment/config/WebAppConfig.java
@@ -1,5 +1,7 @@
 package lk.ijse.aadspringwebmvcassignment.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -8,5 +10,8 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @ComponentScan(basePackages = "lk.ijse.aadspringwebmvcassignment")
 @EnableWebMvc
 public class WebAppConfig {
-
+    static Logger logger = LoggerFactory.getLogger(WebAppConfig.class);
+    public WebAppConfig() {
+        logger.info("WebAppConfig Constructor");
+    }
 }
--- src/main/java/lk/ijse/aadspringwebmvcassignment/config/WebAppRootConfig.java
+++ src/main/java/lk/ijse/aadspringwebmvcassignment/config/WebAppRootConfig.java
@@ -1,9 +1,16 @@
 package lk.ijse.aadspringwebmvcassignment.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan(basePackages = "lk.ijse.aadspringwebmvcassignment")
 public class WebAppRootConfig {
+    static Logger logger = LoggerFactory.getLogger(WebAppRootConfig.class);
+
+    public WebAppRootConfig() {
+        logger.info("WebAppRootConfig initialized");
+    }
 }
