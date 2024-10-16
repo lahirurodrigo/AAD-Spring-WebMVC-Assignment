b7693d2f6181271400587ec697ce3d1d545cbae5
lahirurodrigo <kmlahirurodrigo@gmail.com>
2024-10-16 13:15:00 +0530

    Create HealthCheckController class for check server is working

---
+++ src/main/java/lk/ijse/aadspringwebmvcassignment/controller/HealthCheckController.java
@@ -0,0 +1,14 @@
+package lk.ijse.aadspringwebmvcassignment.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/health")
+public class HealthCheckController {
+    @GetMapping
+    public String healthTest(){
+        return "Pos controller is working";
+    }
+}
